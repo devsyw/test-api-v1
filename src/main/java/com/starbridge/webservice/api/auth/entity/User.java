264d99c0ec745f2d6ae3d81caf53720dd8bb6794
서영우 <seo_youngwoo02@eland.co.kr>
2024-11-07 10:28:28 +0900

    [feat] User Entity refactor by Enum

--- src/main/java/com/starbridge/webservice/api/auth/entity/User.java
+++ src/main/java/com/starbridge/webservice/api/auth/entity/User.java
@@ -1,5 +1,7 @@
 package com.starbridge.webservice.api.auth.entity;
 
+import com.starbridge.webservice.api.auth.entity.type.Gender;
+import com.starbridge.webservice.api.auth.entity.type.Platform;
 import com.starbridge.webservice.api.auth.entity.type.UserType;
 import com.starbridge.webservice.common.BaseEntity;
 import jakarta.persistence.Entity;
@@ -22,11 +24,17 @@ public class User extends BaseEntity {
     private String name; // 이름
     private String email; // 이메일
     private String phoneNumber; // 전화번호
-    private String platform; // 유입 플랫폼 (OAuth2 provider)
+
+    @Enumerated(EnumType.STRING)
+    private Platform platform; // 유입 플랫폼 (OAuth2 provider, Enum)
 
     private String accountNumber; // 계좌번호
+
     @Enumerated(EnumType.STRING)
     private UserType userType; // 회원 유형 (Enum)
-    private String gender; // 성별
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender; // 성별 (Enum)
+
     private String category; // 카테고리
 }
